6fc0cf4b46b5fbbf696c008bdb20f30f8506059a
boomirajp <boomirajp.btech@gmail.com>
2025-02-06 01:32:38 +0530

    Update Az204HelloworldApplication.java

--- src/main/java/com/java/az204_helloworld/Az204HelloworldApplication.java
+++ src/main/java/com/java/az204_helloworld/Az204HelloworldApplication.java
@@ -2,12 +2,20 @@ package com.java.az204_helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Az204HelloworldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Az204HelloworldApplication.class, args);
+	}
+
+	@GetMapping("hello")
+	public String hello() {
+		return "Hello world!";
 	}
 
 }
