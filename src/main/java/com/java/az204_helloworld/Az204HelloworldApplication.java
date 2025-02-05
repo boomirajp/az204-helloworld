cc7f3d82a6e5922ecd01eaa411657fc6c53c8aaf
boomirajp <boomirajp.btech@gmail.com>
2025-02-06 01:30:40 +0530

    Add files via upload
    initial commit

---
+++ src/main/java/com/java/az204_helloworld/Az204HelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.java.az204_helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Az204HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Az204HelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/az204_helloworld/Az204HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.az204_helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Az204HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
